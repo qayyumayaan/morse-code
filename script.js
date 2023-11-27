6528dfd2965086bbccdb19399f696981709743fb
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-11-26 21:09:18 -0500

    Printing behavior (raw)

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let context = new AudioContext();
 let oscillator;
+let keyDownTime;
 
 document.addEventListener('keydown', function(event) {
     if (event.code === 'Space' && !oscillator) {
+        keyDownTime = new Date().getTime(); // Record the time when the key is pressed down
+
         oscillator = context.createOscillator();
         oscillator.frequency.setValueAtTime(500, context.currentTime); // Set frequency to 500 Hz
         oscillator.connect(context.destination);
@@ -12,8 +15,28 @@ document.addEventListener('keydown', function(event) {
 
 document.addEventListener('keyup', function(event) {
     if (event.code === 'Space' && oscillator) {
+        const keyUpTime = new Date().getTime(); // Record the time when the key is released
+        const duration = keyUpTime - keyDownTime; // Calculate the duration of the key press
+
+        if (duration <= 250) {
+            printMessage("DOT");
+        } else {
+            printMessage("DASH");
+        }
+
         oscillator.stop();
         oscillator.disconnect();
         oscillator = null;
+    }
+});
+
+function printMessage(message) {
+    const mainContent = document.getElementById('mainContent');
+    mainContent.innerHTML += `<p>${message}</p>`; // Append the message to the main content
+}
+
+document.addEventListener('keyup', function(event) {
+    if (event.code !== 'Space') {
+        printMessage(" ");
     }
 });
